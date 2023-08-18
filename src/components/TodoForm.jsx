1af45561b2f88abc8995ec65f759452f5ad9e452
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2023-08-17 22:29:36 -0300

    feat(style): v0.10.000 - implements function that ‘Create task’ button send message to console

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 
 const TodoForm = () => {
+  const [value, setValue] = useState('')
+  const [category, setCategory] = useState('')
+
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    console.log("Enviou form!");
+};
+
+
   return (
-    <div className="todo-form">
-    <h2>Criar tarefa</h2>
-    <form>
-       <input type="text" placeholder="Digite o título da tarefa" />
-       <select>
-           <option value="">Selecione uma categoria</option>
-           <option value="Trabalho">Trabalho</option>
-           <option value="Pessoal">Pessoal</option>
-           <option value="Estudos">Estudos</option>
-       </select>
-       <button type="submit">Criar tarefa</button>
-    </form>
- </div>   
+    <div className='todo-form'>
+      <h2>Criar tarefa</h2>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Digite o título da tarefa' />
+        <select>
+          <option value=''>Selecione uma categoria</option>
+          <option value='Trabalho'>Trabalho</option>
+          <option value='Pessoal'>Pessoal</option>
+          <option value='Estudos'>Estudos</option>
+        </select>
+        <button type='submit'>Criar tarefa</button>
+      </form>
+    </div>
   )
 }
 
