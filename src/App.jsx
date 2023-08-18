1bf06667e9eddfd0e37a7cd726d2448b8797c6ce
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2023-08-17 21:37:11 -0300

    feat(react): v00.05.000 - create Todo and TodoForm components and ADD to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
-import "./App.css";
+import { useState } from 'react'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
+import './App.css'
 
 function App () {
   const [todos, setTodos] = useState([
@@ -28,18 +30,10 @@ function App () {
       <h1>Lista de Tarefas</h1>
       <div className='todo-list'>
         {todos.map(todo => (
-          <div className='todo'>
-            <div className='content'>
-              <p>{todo.text}</p>
-              <p className='category'>({todo.category})</p>
-            </div>
-            <div>
-              <button>Completar</button>
-              <button>X</button>
-            </div>
-          </div>
+          <Todo todo={todo} />
         ))}
       </div>
+      <TodoForm />
     </div>
   )
 }
